b043310f9d3e6f787d4ca5eb22f8bd8ec1ab7657
Josephine Chong <josephine.yt.chong@gmail.com>
2019-11-18 01:28:47 +1100

    removed api food puppy

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -12,14 +12,6 @@ class PlantsController < ApplicationController
   def show
     @comments = @plant.comments.all
     @comment = @plant.comments.build
-
-    q = params[:q]
-    return unless q.present?
-    
-    require 'net/http'
-    uri = URI.parse("http://www.recipepuppy.com/api/?" + { q: q, type: 'title' }.to_query)
-    json = Net::HTTP.get(uri)
-    @results = JSON.parse(json)["results"]
   end
 
   # GET /plants/new
